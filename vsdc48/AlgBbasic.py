bbb66f7cdafd7a6ccaca7418dcf6048e4bcfe994
Michal Pluta <vsdc48@durham.ac.uk>
2024-02-03 17:04:56 +0000

    Update vars in code files

--- vsdc48/AlgBbasic.py
+++ vsdc48/AlgBbasic.py
@@ -268,7 +268,7 @@ print("The codes and tariffs have been read from 'alg_codes_and_tariffs.txt':")
 ############
 ############ END OF SECTOR 5 (IGNORE THIS COMMENT)
 
-my_user_name = "abcd12"
+my_user_name = "vsdc48"
 
 ############ START OF SECTOR 6 (IGNORE THIS COMMENT)
 ############
@@ -281,8 +281,8 @@ my_user_name = "abcd12"
 ############
 ############ END OF SECTOR 6 (IGNORE THIS COMMENT)
 
-my_first_name = ""
-my_last_name = ""
+my_first_name = "Michal"
+my_last_name = "Pluta"
 
 ############ START OF SECTOR 7 (IGNORE THIS COMMENT)
 ############
@@ -292,7 +292,7 @@ my_last_name = ""
 ############
 ############ END OF SECTOR 7 (IGNORE THIS COMMENT)
 
-algorithm_code = "XX"
+algorithm_code = "PS"
 
 ############ START OF SECTOR 8 (IGNORE THIS COMMENT)
 ############
